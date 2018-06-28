0e996052eebd04cb23c60df8e9a07e7b415ca758
Filip Chalupa <chalupa.filip@gmail.com>
2018-06-29 01:21:05 +0200

    Deploy to gh-pages config

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,45 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-const IS_PROD = process.env.NODE_ENV === 'production'
+module.exports = args => {
+	const IS_PROD = args.NODE_ENV === 'production'
 
-module.exports = {
-	entry: './src/app.js',
-	output: {
-		filename: '[name].[hash].js',
-		path: path.resolve(__dirname, 'dist'),
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: [
-					'style-loader',
-					{
-						loader: 'css-loader',
-						options: {
-							modules: true,
-							localIdentName: IS_PROD
-								? '[hash:base64:5]'
-								: '[local]--[hash:base64:5]',
+	return {
+		mode: IS_PROD ? 'production' : 'development',
+		entry: './src/app.js',
+		output: {
+			filename: '[name].[hash].js',
+			path: path.resolve(__dirname, 'dist'),
+			publicPath: IS_PROD ? '/where-is/' : '/',
+		},
+		module: {
+			rules: [
+				{
+					test: /\.css$/,
+					use: [
+						'style-loader',
+						{
+							loader: 'css-loader',
+							options: {
+								modules: true,
+								localIdentName: IS_PROD
+									? '[hash:base64:5]'
+									: '[local]--[hash:base64:5]',
+							},
 						},
-					},
-				],
-			},
+					],
+				},
+			],
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				title: 'Kde je brno?',
+				meta: {
+					viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no',
+					description: 'Kterým směrem se vydat a nevydat, když je nejhůře?',
+				},
+				filename: 'index.html',
+			}),
 		],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			title: 'Kde je brno?',
-			meta: {
-				viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no',
-				description: 'Kterým směrem se vydat a nevydat, když je nejhůře?',
-			},
-			filename: 'index.html',
-		}),
-	],
+	}
 }
